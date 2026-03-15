20c7b0b48092c7480778f4479bec23588f7246c5
Thu Vân <162585424+ThuVan0610@users.noreply.github.com>
2026-03-16 00:57:57 +0700

    app cộng phần 2

--- AppCong/app/src/main/java/van/edu/appcong/MainActivity.java
+++ AppCong/app/src/main/java/van/edu/appcong/MainActivity.java
@@ -1,6 +1,8 @@
 package van.edu.appcong;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,11 +16,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        // gắn layout tương ứng với file này
+
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    // đây là bộ lắng nghe và xử lý sự kiện click nút tính tổng
+    public void XuLyCong(View view){
+    // tìm và tham chiếu đến điều khiển trên tệp XML, mapping sang java
+        EditText editTextSoA = findViewById(R.id.edtA);
+        EditText editTextSoB = findViewById(R.id.edtB);
+        EditText editTextKetQua = findViewById(R.id.edtKQ);
+    // lấy dữ liệu về ở điều khiển số a
+        String strA = editTextSoA.getText().toString(); //strA = "2"
+    // lấy dữ liệu về ở điều khiển số b
+        String strB = editTextSoB.getText().toString(); //strB = "4"
+    // Chuyển dữ liệu sang dạng số
+        int so_A = Integer.parseInt(strA);
+        int so_B = Integer.parseInt(strB);
+    // Tính toán theo yêu cầu
+        int tong = so_A + so_B;
+        String strTong = String.valueOf(tong); // chuyển sang dạng chuỗi
+    // Hiện ra màn hình
+        editTextKetQua.setText(strTong);
     }
 }
