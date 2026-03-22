d33ac3868db399a64edff97a006cf84335baeec7
Thu Vân <162585424+ThuVan0610@users.noreply.github.com>
2026-03-23 01:10:37 +0700

    p2

--- VD2/app/src/main/java/van/edu/myapplication/MainActivity.java
+++ VD2/app/src/main/java/van/edu/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package van.edu.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +11,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editTextSo1;
+    EditText editTextSo2;
+    EditText editTextKq;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,84 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TimDieuKien();
+    }
+    void TimDieuKien(){
+        EditText editTextSo1 = findViewById(R.id.edtSo1);
+        EditText editTextSo2 = findViewById(R.id.edtSo2);
+        EditText editTextKQ = findViewById(R.id.edtKetQua);
+    }
+    // xử lý cộng
+    void XuLyCong(View v){
+        EditText editTextSo1 = findViewById(R.id.edtSo1);
+        EditText editTextSo2 = findViewById(R.id.edtSo2);
+
+        String sothu1 = editTextSo1.getText().toString();
+        String sothu2 = editTextSo2.getText().toString();
+
+        float soA = Float.parseFloat(sothu1);
+        float soB = Float.parseFloat(sothu2);
+
+        float Tong = soA + soB;
+
+        EditText editTextKQ = findViewById(R.id.edtKetQua);
+
+        String chuoiKQ = String.valueOf(Tong);
+
+        editTextKQ.setText(chuoiKQ);
+    }
+    void XuLyTru(View v){
+        EditText editTextSo1 = findViewById(R.id.edtSo1);
+        EditText editTextSo2 = findViewById(R.id.edtSo2);
+
+        String sothu1 = editTextSo1.getText().toString();
+        String sothu2 = editTextSo2.getText().toString();
+
+        float soA = Float.parseFloat(sothu1);
+        float soB = Float.parseFloat(sothu2);
+
+        float Tong = soA - soB;
+
+        EditText editTextKQ = findViewById(R.id.edtKetQua);
+
+        String chuoiKQ = String.valueOf(Tong);
+
+        editTextKQ.setText(chuoiKQ);
+    }
+    void XuLyNhan(View v){
+        EditText editTextSo1 = findViewById(R.id.edtSo1);
+        EditText editTextSo2 = findViewById(R.id.edtSo2);
+
+        String sothu1 = editTextSo1.getText().toString();
+        String sothu2 = editTextSo2.getText().toString();
+
+        float soA = Float.parseFloat(sothu1);
+        float soB = Float.parseFloat(sothu2);
+
+        float Tong = soA * soB;
+
+        EditText editTextKQ = findViewById(R.id.edtKetQua);
+
+        String chuoiKQ = String.valueOf(Tong);
+
+        editTextKQ.setText(chuoiKQ);
+    }
+    void XuLyChia(View v){
+        EditText editTextSo1 = findViewById(R.id.edtSo1);
+        EditText editTextSo2 = findViewById(R.id.edtSo2);
+
+        String sothu1 = editTextSo1.getText().toString();
+        String sothu2 = editTextSo2.getText().toString();
+
+        float soA = Float.parseFloat(sothu1);
+        float soB = Float.parseFloat(sothu2);
+
+        float Tong = soA / soB;
+
+        EditText editTextKQ = findViewById(R.id.edtKetQua);
+
+        String chuoiKQ = String.valueOf(Tong);
+
+        editTextKQ.setText(chuoiKQ);
     }
 }
