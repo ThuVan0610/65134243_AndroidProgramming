c58512f4e0f657b35249401cbd6c26543c747678
Thu Vân <162585424+ThuVan0610@users.noreply.github.com>
2026-04-20 08:31:57 +0700

    up

--- thigk2NguyenThiThuVan/app/src/main/java/van/edu/thigk2nguyenthithuvan/MainActivity.java
+++ thigk2NguyenThiThuVan/app/src/main/java/van/edu/thigk2nguyenthithuvan/MainActivity.java
@@ -1,7 +1,6 @@
 package van.edu.thigk2nguyenthithuvan;
 
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
