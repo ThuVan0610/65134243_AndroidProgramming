74ef00dcc62e90ff639703b4f0da9bbe7e8f7037
Thu Vân <162585424+ThuVan0610@users.noreply.github.com>
2026-04-20 08:36:11 +0700

    up

--- thigk2NguyenThiThuVan/app/src/main/java/van/edu/thigk2nguyenthithuvan/MainActivity.java
+++ thigk2NguyenThiThuVan/app/src/main/java/van/edu/thigk2nguyenthithuvan/MainActivity.java
@@ -1,29 +1,34 @@
-package van.edu.thigk2nguyenthithuvan;
+package van.edu.thigk2nguyenthithuvan
 
-import android.os.Bundle;
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-public class MainActivity extends AppCompatActivity {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.btnCau1).setOnClickListener {
+        val btnCau1 = findViewById<Button>(R.id.btnCau1)
+                val btnCau2 = findViewById<Button>(R.id.btnCau2)
+                val btnCau3 = findViewById<Button>(R.id.btnCau3)
+                val btnCau4 = findViewById<Button>(R.id.btnCau4)
+
+                btnCau1.setOnClickListener {
             startActivity(Intent(this, Cau1Activity::class.java))
         }
-        findViewById<Button>(R.id.btnCau2).setOnClickListener {
+
+        btnCau2.setOnClickListener {
             startActivity(Intent(this, Cau2Activity::class.java))
         }
-        findViewById<Button>(R.id.btnCau3).setOnClickListener {
+
+        btnCau3.setOnClickListener {
             startActivity(Intent(this, Cau3Activity::class.java))
         }
-        findViewById<Button>(R.id.btnCau4).setOnClickListener {
+
+        btnCau4.setOnClickListener {
             startActivity(Intent(this, Cau4Activity::class.java))
         }
     }
